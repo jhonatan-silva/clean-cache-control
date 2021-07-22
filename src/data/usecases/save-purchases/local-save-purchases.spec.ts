517611cfc5c300b29b8024cae1dc0898581cc69e
Jhonatan Henrique <jhonatan.henrisilva@gmail.com>
2021-07-22 11:03:31 -0300

    Testa se houve erros quando inserir o cache

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -25,6 +25,12 @@ class CacheStoreSpy implements CacheStore {
             throw new Error()
         })
     }
+    
+    simulateInsertError(): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+            throw new Error()
+        })
+    }
 }
 
 type SutTypes = {
@@ -72,9 +78,7 @@ describe('LocalSavePurchages', () => {
 
     test('Should not insert new cache if delete fails', () => {
         const {cacheStore, sut} = makeSut()
-
         cacheStore.simulateDeleteError()
-
         const promise = sut.save(mockPurchases())
 
         expect(cacheStore.insertCallsCount).toBe(0)
@@ -84,12 +88,19 @@ describe('LocalSavePurchages', () => {
     test('Should insert new cache if delete succeeds', async () => {
         const {cacheStore, sut} = makeSut()
         const purchases = mockPurchases()
-
         await sut.save(purchases)
 
         expect(cacheStore.deleteCallsCount).toBe(1)
         expect(cacheStore.insertCallsCount).toBe(1)
         expect(cacheStore.insertKey).toBe('purchases')
         expect(cacheStore.insertValues).toEqual(purchases)
+    })
+
+    test('Should throw if insert throws', () => {
+        const {cacheStore, sut} = makeSut()
+        cacheStore.simulateInsertError()
+        const promise = sut.save(mockPurchases())
+
+        expect(promise).rejects.toThrow()
     })
 })
